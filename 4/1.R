3ac9ba04a09c2f9a5cfb8a4cfb3020179aa32eda
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-02-02 10:42:32 +0100

    Hypothesis testing

---
+++ 4/1.R
@@ -0,0 +1,13 @@
+library(datasets)
+
+t.test(beaver1$temp, alternative = "two.sided", mu=38.00)
+
+t.test(beaver1$temp, alternative = "two.sided", mu=37.00)
+
+t.test(beaver1$temp, alternative = "two.sided", mu=36.9)
+
+t.test(beaver1$temp, alternative = "two.sided", mu=36.86)
+
+t.test(beaver1$temp, alternative = "two.sided", mu=36.86219)
+
+
